6cf8ffb89d37859746d07192d1eae076a2b13d30
willpotter1 <willfspot@yahoo.co.uk>
2025-03-26 14:41:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   server: {
     host: "::",
     port: 8080,
